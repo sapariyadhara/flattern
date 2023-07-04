c24d4df8c6b358abcf3d54f42e68a3ef1db3cf3a
sapariyadhara <sapariyadhara935@gmail.com>
2023-07-04 08:38:01 +0530

    row colum

--- src/users/testimonials/ListPartners.js
+++ src/users/testimonials/ListPartners.js
@@ -3,19 +3,19 @@ import CustomCard from '../components/Ui/CustomCard';
 
 function ListPartners({tmData}) {
     return (
-        <div>
+        <>
            
                 {
                     tmData.map((v , i) => {
                         return(
-                            <div className="col-lg-6" data-aos="fade-up">
+                            <div className="col-lg-5" data-aos="fade-up">
                                 <CustomCard value={v} />
                             </div>
                         )
                     })
                 }
           
-        </div>
+        </>
     );
 }
 
